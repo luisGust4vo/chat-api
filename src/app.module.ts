7d34827b5e379789cd7fd4f9b8f32c955dc97cb0
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2024-09-16 18:33:08 -0300

    Feat(Api socket): Criar ServerWeb socket para mesagens vinda do front. -Foi instaladi a lib gateway, em seguida foram feitas os ajustes de conecxao.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
-
+import { AppGateway } from './app/app.gateway';
 @Module({
   imports: [],
   controllers: [],
-  providers: [AppService],
+  providers: [AppGateway],
 })
 export class AppModule {}
